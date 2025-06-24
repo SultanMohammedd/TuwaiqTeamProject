0c1b85841c46ba8fc2a5eb672cdf36ffd243ba90
Sultan Mohammed <sult10.sa@gmail.com>
2025-06-24 22:06:39 +0300

    Player Movement

--- Assets/PlayerMovment.cs
+++ Assets/PlayerMovment.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    public float playerSpeed;
+    public float jumpForce;
+    public Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,46 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-         
+
+
+        float hor = Input.GetAxis("Horizontal") *playerSpeed;
+        float ver = Input.GetAxis("Vertical") * playerSpeed;
+        rb.linearVelocity = new Vector3(hor, rb.linearVelocity.y, ver);
+        if (transform.position.y < 1.1) {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+
+                rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+            } 
+        }
+        //if (Input.GetKey(KeyCode.W))
+        //{
+
+        //    //transform.position += new Vector3(0, 0, playerSpeed);
+        //    rb.linearVelocity += new Vector3(0, 0, playerSpeed);
+        //}
+
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    transform.position += new Vector3(0, 0, - playerSpeed);
+        //}
+
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    transform.position += new Vector3(- playerSpeed, 0, 0);
+        //}
+
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    transform.position += new Vector3(playerSpeed, 0, 0);
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+
+
+        //    Rigidbody body = GetComponent<Rigidbody>();
+        //    body.AddForce(Vector3.up * jumpForce, ForceMode.Force);
+        //}
     }
 }
