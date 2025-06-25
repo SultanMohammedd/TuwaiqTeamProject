8c57397d930f82e31a804262f3e17c60ce974f98
Sultan Mohammed <sult10.sa@gmail.com>
2025-06-25 19:06:32 +0300

    Jump and Run code

--- Assets/PlayerMovment.cs
+++ Assets/PlayerMovment.cs
@@ -5,55 +5,48 @@ public class NewMonoBehaviourScript : MonoBehaviour
     public float playerSpeed;
     public float jumpForce;
     public Rigidbody rb;
+    public Collider Collider;
+    public bool jumpAllowed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
-        float hor = Input.GetAxis("Horizontal") *playerSpeed;
+        float hor = Input.GetAxis("Horizontal") * playerSpeed;
         float ver = Input.GetAxis("Vertical") * playerSpeed;
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            playerSpeed = 15;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            playerSpeed = 5;
+        }
         rb.linearVelocity = new Vector3(hor, rb.linearVelocity.y, ver);
-        if (transform.position.y < 1.1) {
+        if (jumpAllowed)
+        {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-
                 rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
-            } 
+            }
         }
-        //if (Input.GetKey(KeyCode.W))
-        //{
-
-        //    //transform.position += new Vector3(0, 0, playerSpeed);
-        //    rb.linearVelocity += new Vector3(0, 0, playerSpeed);
-        //}
-
-        //if (Input.GetKey(KeyCode.S))
-        //{
-        //    transform.position += new Vector3(0, 0, - playerSpeed);
-        //}
-
-        //if (Input.GetKey(KeyCode.A))
-        //{
-        //    transform.position += new Vector3(- playerSpeed, 0, 0);
-        //}
-
-        //if (Input.GetKey(KeyCode.D))
-        //{
-        //    transform.position += new Vector3(playerSpeed, 0, 0);
-        //}
-
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-
-
-        //    Rigidbody body = GetComponent<Rigidbody>();
-        //    body.AddForce(Vector3.up * jumpForce, ForceMode.Force);
-        //}
+       
     }
+    public void OnTriggerEnter(Collider other)
+    {
+        
+            jumpAllowed = true;
+        
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        
+            jumpAllowed = false;
+        
+    }
+
 }
