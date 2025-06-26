f7f88a98e595cb3fe613df4cd67c7602049a5e3a
darealsidi2003 <darealsidi2003@icloud.com>
2025-06-26 19:31:28 +0300

    .

--- Assets/Fose/Responrs.cs
+++ Assets/Fose/Responrs.cs
@@ -7,7 +7,7 @@ public class Responrs : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        InvokeRepeating("SpawnEnemys",0,0.50f);
+        InvokeRepeating("SpawnEnemys",0,2f);
     }
 
     // Update is called once per frame
